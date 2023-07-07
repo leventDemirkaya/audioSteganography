0b7ea28457ff6ee793140e194955ef6e517abc64
leventDemirkaya <leventdemirkayakku@gmail.com>
2023-07-07 12:13:18 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+namespace SesVerileriÜzerineVeriGizleme
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.sesOynatici = new AxWMPLib.AxWindowsMediaPlayer();
+            this.lstbx_SesDosyalari = new System.Windows.Forms.ListBox();
+            this.bt_SesDosyasiEkle = new System.Windows.Forms.Button();
+            this.lbl_GizlenecekMetin = new System.Windows.Forms.Label();
+            this.txt_GizlenecekMetin = new System.Windows.Forms.TextBox();
+            this.bt_MesajGizle = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.gizlenmisMesajOynatici = new AxWMPLib.AxWindowsMediaPlayer();
+            this.txt_GizlenenMesaj = new System.Windows.Forms.TextBox();
+            this.lbl_GizlenenMesaj = new System.Windows.Forms.Label();
+            this.Zamanlayici = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.sesOynatici)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gizlenmisMesajOynatici)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sesOynatici
+            // 
+            this.sesOynatici.Enabled = true;
+            this.sesOynatici.Location = new System.Drawing.Point(13, 13);
+            this.sesOynatici.Name = "sesOynatici";
+            this.sesOynatici.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("sesOynatici.OcxState")));
+            this.sesOynatici.Size = new System.Drawing.Size(443, 240);
+            this.sesOynatici.TabIndex = 0;
+            // 
+            // lstbx_SesDosyalari
+            // 
+            this.lstbx_SesDosyalari.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.lstbx_SesDosyalari.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstbx_SesDosyalari.FormattingEnabled = true;
+            this.lstbx_SesDosyalari.ItemHeight = 15;
+            this.lstbx_SesDosyalari.Location = new System.Drawing.Point(462, 13);
+            this.lstbx_SesDosyalari.Name = "lstbx_SesDosyalari";
+            this.lstbx_SesDosyalari.Size = new System.Drawing.Size(326, 199);
+            this.lstbx_SesDosyalari.TabIndex = 1;
+            this.lstbx_SesDosyalari.SelectedIndexChanged += new System.EventHandler(this.lstbx_SesDosyalari_SelectedIndexChanged);
+            // 
+            // bt_SesDosyasiEkle
+            // 
+            this.bt_SesDosyasiEkle.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.bt_SesDosyasiEkle.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_SesDosyasiEkle.Location = new System.Drawing.Point(462, 218);
+            this.bt_SesDosyasiEkle.Name = "bt_SesDosyasiEkle";
+            this.bt_SesDosyasiEkle.Size = new System.Drawing.Size(326, 35);
+            this.bt_SesDosyasiEkle.TabIndex = 2;
+            this.bt_SesDosyasiEkle.Text = "Ses Dosyası Ekle";
+            this.bt_SesDosyasiEkle.UseVisualStyleBackColor = false;
+            this.bt_SesDosyasiEkle.Click += new System.EventHandler(this.bt_SesDosyasiEkle_Click);
+            // 
+            // lbl_GizlenecekMetin
+            // 
+            this.lbl_GizlenecekMetin.AutoSize = true;
+            this.lbl_GizlenecekMetin.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lbl_GizlenecekMetin.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_GizlenecekMetin.Location = new System.Drawing.Point(9, 273);
+            this.lbl_GizlenecekMetin.Name = "lbl_GizlenecekMetin";
+            this.lbl_GizlenecekMetin.Size = new System.Drawing.Size(228, 19);
+            this.lbl_GizlenecekMetin.TabIndex = 3;
+            this.lbl_GizlenecekMetin.Text = "Gizlenecek Metni Giriniz : ";
+            this.lbl_GizlenecekMetin.Visible = false;
+            // 
+            // txt_GizlenecekMetin
+            // 
+            this.txt_GizlenecekMetin.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txt_GizlenecekMetin.Enabled = false;
+            this.txt_GizlenecekMetin.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_GizlenecekMetin.Location = new System.Drawing.Point(231, 270);
+            this.txt_GizlenecekMetin.MaxLength = 17;
+            this.txt_GizlenecekMetin.Name = "txt_GizlenecekMetin";
+            this.txt_GizlenecekMetin.Size = new System.Drawing.Size(546, 26);
+            this.txt_GizlenecekMetin.TabIndex = 4;
+            this.txt_GizlenecekMetin.Text = "17 Karaktere Kadar Gizleme Yapılabilmektedir.";
+            this.txt_GizlenecekMetin.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_GizlenecekMetin.Visible = false;
+            this.txt_GizlenecekMetin.MouseClick += new System.Windows.Forms.MouseEventHandler(this.txt_GizlenecekMetin_MouseClick);
+            // 
+            // bt_MesajGizle
+            // 
+            this.bt_MesajGizle.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.bt_MesajGizle.Enabled = false;
+            this.bt_MesajGizle.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_MesajGizle.Location = new System.Drawing.Point(380, 302);
+            this.bt_MesajGizle.Name = "bt_MesajGizle";
+            this.bt_MesajGizle.Size = new System.Drawing.Size(225, 35);
+            this.bt_MesajGizle.TabIndex = 5;
+            this.bt_MesajGizle.Text = "Mesajı Gizle";
+            this.bt_MesajGizle.UseVisualStyleBackColor = false;
+            this.bt_MesajGizle.Visible = false;
+            this.bt_MesajGizle.Click += new System.EventHandler(this.bt_MesajGizle_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // gizlenmisMesajOynatici
+            // 
+            this.gizlenmisMesajOynatici.Enabled = true;
+            this.gizlenmisMesajOynatici.Location = new System.Drawing.Point(12, 343);
+            this.gizlenmisMesajOynatici.Name = "gizlenmisMesajOynatici";
+            this.gizlenmisMesajOynatici.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("gizlenmisMesajOynatici.OcxState")));
+            this.gizlenmisMesajOynatici.Size = new System.Drawing.Size(765, 240);
+            this.gizlenmisMesajOynatici.TabIndex = 6;
+            this.gizlenmisMesajOynatici.Visible = false;
+            // 
+            // txt_GizlenenMesaj
+            // 
+            this.txt_GizlenenMesaj.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txt_GizlenenMesaj.Enabled = false;
+            this.txt_GizlenenMesaj.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_GizlenenMesaj.Location = new System.Drawing.Point(192, 598);
+            this.txt_GizlenenMesaj.MaxLength = 17;
+            this.txt_GizlenenMesaj.Name = "txt_GizlenenMesaj";
+            this.txt_GizlenenMesaj.ReadOnly = true;
+            this.txt_GizlenenMesaj.Size = new System.Drawing.Size(585, 26);
+            this.txt_GizlenenMesaj.TabIndex = 8;
+            this.txt_GizlenenMesaj.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_GizlenenMesaj.Visible = false;
+            // 
+            // lbl_GizlenenMesaj
+            // 
+            this.lbl_GizlenenMesaj.AutoSize = true;
+            this.lbl_GizlenenMesaj.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lbl_GizlenenMesaj.Enabled = false;
+            this.lbl_GizlenenMesaj.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_GizlenenMesaj.Location = new System.Drawing.Point(9, 601);
+            this.lbl_GizlenenMesaj.Name = "lbl_GizlenenMesaj";
+            this.lbl_GizlenenMesaj.Size = new System.Drawing.Size(177, 19);
+            this.lbl_GizlenenMesaj.TabIndex = 7;
+            this.lbl_GizlenenMesaj.Text = "Gizlediginiz Mesaj : ";
+            this.lbl_GizlenenMesaj.Visible = false;
+            // 
+            // Zamanlayici
+            // 
+            this.Zamanlayici.Interval = 600000;
+            this.Zamanlayici.Tick += new System.EventHandler(this.Zamanlayici_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(789, 640);
+            this.Controls.Add(this.txt_GizlenenMesaj);
+            this.Controls.Add(this.lbl_GizlenenMesaj);
+            this.Controls.Add(this.gizlenmisMesajOynatici);
+            this.Controls.Add(this.bt_MesajGizle);
+            this.Controls.Add(this.txt_GizlenecekMetin);
+            this.Controls.Add(this.lbl_GizlenecekMetin);
+            this.Controls.Add(this.bt_SesDosyasiEkle);
+            this.Controls.Add(this.lstbx_SesDosyalari);
+            this.Controls.Add(this.sesOynatici);
+            this.Name = "Form1";
+            this.Text = "Ses Verileri Üzerine Veri Gizleme";
+            ((System.ComponentModel.ISupportInitialize)(this.sesOynatici)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gizlenmisMesajOynatici)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer sesOynatici;
+        private System.Windows.Forms.ListBox lstbx_SesDosyalari;
+        private System.Windows.Forms.Button bt_SesDosyasiEkle;
+        private System.Windows.Forms.Label lbl_GizlenecekMetin;
+        private System.Windows.Forms.TextBox txt_GizlenecekMetin;
+        private System.Windows.Forms.Button bt_MesajGizle;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private AxWMPLib.AxWindowsMediaPlayer gizlenmisMesajOynatici;
+        private System.Windows.Forms.TextBox txt_GizlenenMesaj;
+        private System.Windows.Forms.Label lbl_GizlenenMesaj;
+        private System.Windows.Forms.Timer Zamanlayici;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+using System.IO; // Ses Dosyasından Okuma İşleminde Kullanılmaktadır.
+using NAudio.Wave; // Wav Dosyasından Okuma İşleminde Kullanılmaktadır.
+
+namespace SesVerileriÜzerineVeriGizleme
+{
+    public partial class Form1 : Form
+    {
+        List<decimal> fibonacciNumbers = new List<decimal>(); // daha fazla karakter gizleyebilmek için veri tipi decimal seçildi.
+        List<string> sifirlar = new List<string> { "", "0", "00", "000", "0000", "00000", "000000", "0000000", "00000000" }; // ASCII koduna göre 8 bite tamamlanacak.
+        List<string> path = new List<string>(); // Eklenen ses dosyalarının adresi bulunmaktadır
+        List<string> sesDosyasiIkiTabani = new List<string>(); //sesDosyasındaki verinin binary değerleri bulunmaktadır
+        List<string> saklanacakMesajIkiTabani = new List<string>(); //gizlenecek metinin karakterlerinin binary karşılıkları bulunmaktadır
+        List<int> kullanilanIndisler = new List<int>(); // sırasıyla gizleme yapılan bitlerin bulunduğu indisler
+        byte[] sesDosyasiOnTabani = null; // gizlenmiş mesajı içeren .wav formatına çevrilmede kullanılacak değişken
+        int calistirilmaSayisi = 0; // kaç kez forms'un çalıştığının takibi için kullanılacaktır.
+        public Form1()
+        {
+            InitializeComponent();
+            FibonacciOlustur();
+        }
+        private void bt_SesDosyasiEkle_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            for (int i = 0; i < openFileDialog1.SafeFileNames.Length; i++)
+                if (!lstbx_SesDosyalari.Items.Contains(openFileDialog1.SafeFileNames[i].ToString())) // Eğer Listbox'ta seçilen sesDosyası mevcut değilse ekleyecek
+                {
+                    lstbx_SesDosyalari.Items.Add(openFileDialog1.SafeFileNames[i].ToString()); // SafeFileNames ile sadece dosyaAdı gelmekte, adresi gelmemektedir.
+                    path.Add(openFileDialog1.FileNames[i].ToString()); // sesDosyasının adresini de FileNames metodu ile path listesine ekle
+                }
+        }
+        private void lstbx_SesDosyalari_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                lbl_GizlenecekMetin.Visible = true; txt_GizlenecekMetin.Visible = true; txt_GizlenecekMetin.Enabled = true;
+                bt_MesajGizle.Visible = true; bt_MesajGizle.Enabled = true;
+                sesOynatici.URL = path[lstbx_SesDosyalari.SelectedIndex]; // listbox'tan seçilen indeksteki sesDosyasının adresi sesOynatıcıya gönderilir.
+                sesOynatici.Ctlcontrols.play();
+                byte[] sesDosyasi = File.ReadAllBytes(path[lstbx_SesDosyalari.SelectedIndex]); // sesDosyasının decimal tabanına dönüşümü sağlanır
+                IkiTabaninaCevir(sesDosyasi, 1); // Gizleme işlemi için 8 bit uzunluğunda binary tabanında sayıya dönüştürülür.
+            }
+            catch(Exception) // listbox içerisinde geçersiz bir alana tıklayınca oluşan exception'ı kontrol etmek için.
+            {
+                MessageBox.Show("Listeden seçim sırasında hata oluştu, tekrar deneyiniz");
+                lbl_GizlenecekMetin.Visible = false; txt_GizlenecekMetin.Visible = false; txt_GizlenecekMetin.Enabled = false;
+                bt_MesajGizle.Visible = false; bt_MesajGizle.Enabled = false; // gizleme yapılacak orijinal dosya doğru seçilmezse gizlenecekMetin özellikleri görünür olmayacak.
+            }
+        }
+        private void IkiTabaninaCevir(byte[] dizi, byte dosyaTipi)
+        {
+            StreamWriter sw = null;
+            int sayac = 0;
+            if (dosyaTipi == 1) // orijinalDosya indisi 1 olarak belirlendi.
+            {
+                sw = new StreamWriter(@"orijinalDosya.txt"); // orijinal ses verilerinin binary formdaki karşılıkları bu .txt uzantısında bulunacak
+                for (int i = 0; i < dizi.Length; i++)
+                {
+                    string temp = Convert.ToString(dizi[i], 2); // decimalden binary'e dönüşüm
+                    if (temp.Length < 8) // 8 bitten kısa ise 8 bite ulaşması için sifirlar listesinden ekleme olacak
+                        temp = sifirlar[8 - temp.Length] + temp;
+                    sw.WriteLine(((++sayac) + ". eleman ---> " + dizi[i] + " : \t" + temp)); // 8 bite ulaşan sesVerisi txt dosyasına yazıldı.
+                    sesDosyasiIkiTabani.Add(temp); // sonuç listesi kullanılarak veriGizleme yapılacaktır.
+                }
+                MessageBox.Show("Orijinal Ses Dosyası Binary Tabana Dönüştürülüp .txt Formatında Yazılmıştır.","İşlem Başarılı");
+            }
+            else if (dosyaTipi == 2)
+            {
+                sw = File.AppendText(@"gizlenmisMesaj.txt"); // gizlenmişMesaj.txt oluşturulup 8 bit binary formdaki gizlenmiş mesaj değerleri tutulacak.
+                String mesaj = txt_GizlenecekMetin.Text;
+                sw.Write($"Gizlenecek Mesaj: {mesaj}\nBinary Olarak Kodlanmış Hali :\n");
+                for (int i = 0; i < mesaj.Length; i++)
+                {
+                    string temp = Convert.ToString(mesaj[i], 2); // dosyaTipi=1 parametresine göre tek değişiklik, dizi[i] değil de mesaj[i] parametre alınıyor.
+                    if (temp.Length < 8) // 8 bitten kısa ise 8 bite ulaşması için sifirlar listesinden ekleme olacak
+                        temp = sifirlar[8 - temp.Length] + temp;
+                    saklanacakMesajIkiTabani.Add(temp);
+                    sw.WriteLine(((++sayac) + ". eleman ---> " + mesaj[i] + " : " + temp));
+                }
+                sw.WriteLine("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
+                MessageBox.Show("Gizlenecek mesaj dosyaya yazılmıştır.","İşlem Başarılı");
+            }
+            sw.Close();
+        }
+        private void OnTabaninaCevir()
+        {
+            sesDosyasiOnTabani = new byte[sesDosyasiIkiTabani.Count];
+            for(int i=0;i<sesDosyasiIkiTabani.Count;i++) 
+            {
+                string temp = ""; byte deger = 0; // iki tabanındaki dosyada değişiklik yapmamak için temp değişkeni tanımlandı
+                for(int j=sesDosyasiIkiTabani[i].Length-1;j>=0;j--) // taban dönüşümü yapmak için string tersten yazıldı
+                    temp += sesDosyasiIkiTabani[i][j];
+                for(int j=0;j<temp.Length;j++) 
+                {
+                    if(temp[j]=='1') // 1 değerine sahipse binary --> decimal dönüşümü 2^j(i'nin basamak sayısı) olarak yapılacak.         
+                        deger += (byte)(Math.Pow(2, j));                    
+                }
+                sesDosyasiOnTabani[i] = deger;
+            }
+        }
+        private void bt_MesajGizle_Click(object sender, EventArgs e)
+        {
+            StreamWriter sw = new StreamWriter(@"degistirilmisDosya.txt"); ;
+            if (txt_GizlenecekMetin.Text != "") // Gizlenecek metin girilmişse çalışacak
+            {
+                byte kontrol = 0; // Türkçe Karakter kullanılmasının kontrolünde kullanılmaktadır.
+                for (byte i = 0; i < txt_GizlenecekMetin.Text.Length; i++)
+                    if (txt_GizlenecekMetin.Text.ToLower()[i] == 'ğ' || txt_GizlenecekMetin.Text.ToLower()[i] == 'ü' || 
+                        txt_GizlenecekMetin.Text.ToLower()[i] == 'ç' || txt_GizlenecekMetin.Text[i] == 'ı' || 
+                        txt_GizlenecekMetin.Text.ToLower()[i] == 'ö' || txt_GizlenecekMetin.Text[i]=='İ')
+                    {
+                        kontrol = 1;
+                        break;
+                    }
+                if (kontrol == 0) // Türkçe karakter kullanılmamışsa iki tabanına çevrilecek.
+                {
+                    IkiTabaninaCevir(null, 2); // eğer boş metin değilse çalışacak
+                    txt_GizlenecekMetin.Enabled = false;
+                    bt_MesajGizle.Enabled = false;
+                }
+                else MessageBox.Show("Türkçe Karakter Kullanılmaması Gereklidir.", "HATA ");
+            }
+            else MessageBox.Show("Gizlenecek metin girilmedi, tekrar deneyiniz.", "HATA !");
+            if (saklanacakMesajIkiTabani.Count != 0) // Gizlenecek metin girilmiş ve 2 tabanına çevrimi sağlanmışsa
+            {
+                LSBGizlemeYap(sw);
+                OnTabaninaCevir();
+                WavDosyasiOlustur();
+                Zamanlayici.Start();
+                DegiskenTemizle();
+            }
+            sw.Close();
+        }
+        private void FibonacciOlustur()
+        {
+            fibonacciNumbers.Add(0); fibonacciNumbers.Add(1); fibonacciNumbers.Add(2);
+            for (int i = 2; i < 136; i++) // 17 karaktere kadar gizleme yapabilecektir. [decimal max.value aşılıyor]
+                fibonacciNumbers.Add(fibonacciNumbers[i] + fibonacciNumbers[i - 1]);
+        }
+        private void LSBGizlemeYap(StreamWriter sw)
+        {
+            int fiboIndis = 0, gizlenmisIndis = 0; // fiboIndis kaç karaktere kadar indexOutOfRange hatası yenmeden çalışacağını tutmakta, gizlenmiş indiste ise kaçıncı 8 bitlik karakterde olunduğunun bilgisi tutulmaktadır.
+            String tempOrijinal, tempGizlenmisMesaj = saklanacakMesajIkiTabani[gizlenmisIndis++]; // tempOrijinal, orijinal ses dosyasında ; tempGizlenmişMesaj ise gizlenecek metin üzerindeki bit işlemlerinde kullanılacaktır.
+            for (int i = 0; fibonacciNumbers[i] < sesDosyasiIkiTabani.Count; i++)
+                fiboIndis++; // orijinal ses dosyasında, fibonacci serisi kullanılarak ulaşılabilecek en son bitin indisi tutulmaktadır.
+            for (int i = 0; i < saklanacakMesajIkiTabani.Count*8; i++) // bit bazlı işlem yapıldığı için 8 bite dönüştürülen gizlenecekMesaj kadar dönecektir.
+            {
+                decimal tutulanFiboDegeri = fibonacciNumbers[i];
+                if (i>=fiboIndis) // fibonacci sınırları orijinal mesaj için outOfRange hatası veriyorsa, düzenleme yapılması gerekli.
+                    tutulanFiboDegeri = (fibonacciNumbers[i] % sesDosyasiIkiTabani.Count);
+                int tempSayac = i % 8;  // i sayacını döngü değişkeni olduğu için güncellenemez, o yüzden tempSayac oluşturuldu.
+                tempOrijinal = sesDosyasiIkiTabani[(int)tutulanFiboDegeri];   // Fibonacci sayısına denk gelen orijinal 8-bitlik dizginin LSB haricindeki dizgisi alınacak.
+                if (i % 8 == 0 && i > 0)    // gizlenecekMesajın ilgili karakterinin bütün bitleri gizlenip sıradaki karaktere geçilecekse
+                    tempGizlenmisMesaj = saklanacakMesajIkiTabani[gizlenmisIndis++];    // gizlenmişIndis değeri arttırılarak sıradaki 8 bit tempGizlenmişMesaj içerisine yazılır.
+                char tempKarakter = tempGizlenmisMesaj[tempSayac];  // Karşılaştırılacak ve uyumsuz olursa değiştirilecek bit değeridir. tempSayac indis olarak alındı, çünkü i%8'den dolayı 0 değerinden başlıyor.
+                if (tempOrijinal[tempOrijinal.Length - 1] != tempKarakter)  // eğer orijinal LSB ile tempKarakter farklıysa, LSB değiştirilecek
+                {
+                    tempOrijinal = tempOrijinal.Substring(0, tempOrijinal.Length - 1);  // orijinalMesajın sadece LSB'si değişeceği için ilk 7 biti korundu.
+                    tempOrijinal += tempKarakter;    // gizlenecekKarakter orijinal mesaja ekleniyor.
+                    sesDosyasiIkiTabani[(int)tutulanFiboDegeri] = tempOrijinal;   // Listedeki indisteki değer de güncelleniyor.
+                } // else yazılmasına gerek olmuyor, değiştirilme olmadığı için orijinal mesajın indisinin tutulması yeterli oluyor.     
+                sw.WriteLine((i + 1) + ".karakter " + tutulanFiboDegeri + ". elemana gizlenmiş olup 8 bit ASCII karşılığı : " + tempOrijinal);
+                kullanilanIndisler.Add((int)tutulanFiboDegeri);
+            }
+        } 
+        private void txt_GizlenecekMetin_MouseClick(object sender, MouseEventArgs e)
+        {
+            txt_GizlenecekMetin.Clear(); // Gizlenebilecek karakter üst limiti mesajı kullanıcı tarafından okunduktan sonra silinmesi sağlanacaktır.
+        }
+        private String GizlenenMesajiGoster()
+        {
+            string sonuc = "",tempTersten="",tempNormal=""; 
+            char karakter; byte deger = 0, limit = 8;
+            for(int i=1;i<=kullanilanIndisler.Count;i++)
+            {
+                karakter = sesDosyasiIkiTabani[kullanilanIndisler[i-1]][sesDosyasiIkiTabani[kullanilanIndisler[i-1]].Length-1];
+                tempTersten += karakter;
+                if(i%limit==0)
+                {
+                    for(int j=tempTersten.Length-1;j>=0;j--)
+                        tempNormal += tempTersten[j];
+                    for(int j=0;j<tempNormal.Length;j++)
+                    {
+                        if(tempNormal[j]=='1')
+                            deger += (byte)Math.Pow(2, j);
+                    }
+                    sonuc += (char)deger;
+                    tempTersten = ""; tempNormal = ""; deger = 0;
+                }
+            }
+            return sonuc;
+        }
+        private void WavDosyasiOlustur()
+        {
+            String dosyaAdi = $"gizlenen{++calistirilmaSayisi}.wav";
+            File.WriteAllBytes(dosyaAdi, sesDosyasiOnTabani);
+            txt_GizlenenMesaj.Text = GizlenenMesajiGoster();
+            lbl_GizlenenMesaj.Visible = true; txt_GizlenenMesaj.Visible = true;
+            sesOynatici.URL = path[lstbx_SesDosyalari.SelectedIndex];
+            if (DosyaGecerliMi(@dosyaAdi))
+            {
+                txt_GizlenenMesaj.Location = new Point(192,598);
+                lbl_GizlenenMesaj.Location = new Point(9,601);
+                this.Size = new Size(805, 679);
+                gizlenmisMesajOynatici.Visible = true;
+                gizlenmisMesajOynatici.URL = dosyaAdi;
+                gizlenmisMesajOynatici.Ctlcontrols.play();
+            }
+            else
+            {
+                txt_GizlenenMesaj.Location = new Point(192,356);
+                lbl_GizlenenMesaj.Location = new Point(12,359);
+                this.Size = new Size(800,438);
+            }
+            sesOynatici.Ctlcontrols.play();
+        }
+        private void Zamanlayici_Tick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private bool DosyaGecerliMi(string adres)
+        {
+            try
+            {
+                using (var oku = new WaveFileReader(adres)) // Dosya başarıyla okunabiliyorsa true return ediyoruz
+                    return true;
+            }
+            catch (Exception) // Dosya okunamadı veya hata oluştuysa false return ediyoruz
+            {
+                return false;
+            }
+        }
+        private void DegiskenTemizle()
+        {
+            // İlk çalıştırmadan sonra yeni değerlerin işlenebilmesi için temizlenmesi gereklidir.
+            sesDosyasiIkiTabani.Clear();
+            saklanacakMesajIkiTabani.Clear();
+            kullanilanIndisler.Clear();
+            sesDosyasiOnTabani = null;
+        }
+    }
+}
